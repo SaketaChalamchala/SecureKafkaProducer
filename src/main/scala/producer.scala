80e53675ca9e6a42498749212df759f42dc6f170
SaketaChalamchala <saketa.chalamchala@gmail.com>
2018-05-22 15:00:20 -0500

    Separated producers

--- src/main/scala/producer.scala
+++ src/main/scala/producer.scala
@@ -1,7 +1,7 @@
 /*
  * Author : Saketa Chalamchala
  *
- * Creates a FileStream of XML data.
+ * Creates a Batch of XML data from local files.
  * Publishes the XML as a single message on a Secure Kafka topic .
  *
  *
@@ -19,12 +19,9 @@ import java.io.{File, FileInputStream}
 import scala.io.Source
 import scala.collection.JavaConverters._
 import org.apache.spark.SparkFiles
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.Row
-import org.apache.spark._
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.StreamingContext._
+
 
 object producer {
 
@@ -47,28 +44,63 @@ object producer {
 
   }
 
-  def main(args: Array[String]): Unit = {
+
+  /* Publishes message from an input stream to Kafka topic
+   * @args :
+   * dataDF - key-value Batch DataFrame to be publish
+   * topic - Kafka destination
+   */
+  def publish_batch( dataDF : DataFrame, topic : String): Unit =
+  {
+     dataDF.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+       .write
+       .format("kafka")
+       .option("kafka.bootstrap.servers", props.getProperty("kafka.bootstrap.servers"))
+       .option("topic", topic)
+       .save()
+  }
+
+  /* Publishes messages to Kafka topic in batch
+   * @args :
+   * dataDF - key-value Batch DataFrame to be publish
+   * topic - Kafka destination
+   */
+  def publish_stream( dataDF : DataFrame, topic : String): Unit =
+  {
+    dataDF.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+      .writeStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", props.getProperty("kafka.bootstrap.servers"))
+      .option("topic", topic)
+      .start()
+  }
+
+  /* Generating input data from sample XML files
+   * @args :
+   *
+   */
+  def generate_input_stream() : DataFrame = {
 
     /* Reading a sample XML file  */
     val inputFileName : String = props.getProperty("input.form.path")
     val schema : StructType = StructType(
-        StructField("key", StringType, true) ::
+      StructField("key", StringType, true) ::
         StructField("value", StringType, false) :: Nil)
 
     /* Creating a DF from XML file  */
-    val ADPForm : String = Source.fromFile(inputFileName).mkString
-    val ADPFormRow  = List(Row("1", ADPForm)).asJava
-    val ADPFormDF = spark.createDataFrame(ADPFormRow, schema)
+    val inputForm : String = Source.fromFile(inputFileName).mkString
+    val inputRow  = List(Row("1", inputForm)).asJava
+    val inputDF = spark.createDataFrame(inputRow, schema)
+    return inputDF
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    /* Generate input data/stream  */
+    val ADPFormDF = generate_input_stream
 
     /* Producing to a Kafka topic  */
-    ADPFormDF.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-      .write
-      .format("kafka")
-      .option("kafka.bootstrap.servers", props.getProperty("kafka.bootstrap.servers"))
-      .option("topic", props.getProperty("kafka.topic"))
-      .save()
-
-
+    publish_batch(ADPFormDF, props.getProperty("kafka.topic"))
 
   }
 }
